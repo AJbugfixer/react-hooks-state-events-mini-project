05ccfa13ec3a1e5bb9b655a7355e3de63979ccd4
Ajay <ajay.ip3105@gmail.com>
2022-06-05 21:51:39 -0400

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,50 @@ import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
+
 import { CATEGORIES, TASKS } from "../data";
+import { useState } from "react";
+
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [category, setCategory] = useState("All")
+
+  const handleDeleteTask = (deletedText) => {
+    const updatedTasks = tasks.filter((task) => task.text !== deletedText);
+    setTasks(updatedTasks)
+  }
+
+  const tasksToDisplay = tasks.filter((task) => {
+    if (category === "All") {
+      return true
+    } else {
+      return task.category === category
+    }
+  })
+
+  const onTaskFormSubmit = (newTask) => {
+    setTasks([...tasks, newTask])
+  }
+  
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+	  <CategoryFilter 
+        categories={CATEGORIES} 
+        selectedCategory={category} 
+        handleCategoryFilter={setCategory} 
+      />
+      <NewTaskForm 
+        categories={CATEGORIES}
+        onTaskFormSubmit={onTaskFormSubmit}
+      />
+      <TaskList 
+        handleDeleteTask={handleDeleteTask} 
+        tasks={tasksToDisplay}
+      />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, handleDeleteTask}) {
+  const tasksToDisplay = tasks.map(task => 
+    <Task
+      key={task.text}
+      text={task.text}
+      category={task.category}
+      handleDeleteTask={handleDeleteTask}
+    />)
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasksToDisplay}
     </div>
   );
 }
